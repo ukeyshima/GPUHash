295f2f098591fce2298bc469ef7a7aab618b21cd
ukeyshima <ukeyshima@gmail.com>
2022-06-20 02:30:22 +0900

    Add Sample Directory

---
+++ Assets/GPUHash/Sample/QuadRenderer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GPUHash.Sample
+{
+    public class QuadRenderer : MonoBehaviour
+    {
+        [SerializeField] private Mesh _quad;
+        [SerializeField] private Material _mat;
+
+        private Matrix4x4 _quadMatrix = Matrix4x4.identity;
+        private Vector2Int _screenResolution = new Vector2Int(0, 0);
+
+        private void Update()
+        {
+            Graphics.DrawMesh(_quad, _quadMatrix, _mat, 0);
+
+            if (_screenResolution.x == Screen.width && _screenResolution.y == Screen.height) return;
+            OnChangedResolution();
+        }
+
+        private void OnChangedResolution()
+        {
+            _screenResolution = new Vector2Int(Screen.width, Screen.height);
+            float aspectRatio = (float)_screenResolution.x / (float)_screenResolution.y;
+            float height = Camera.main.orthographicSize * 2;
+            float width = height * aspectRatio;
+            _quadMatrix.SetTRS(Vector3.zero, Quaternion.identity, new Vector3(width, Camera.main.orthographicSize * 2, 0));
+        }
+    }
+
+}
---
+++ Assets/GPUHash/Sample/Shaders/GPUHashVisualization.shader
@@ -0,0 +1,46 @@
+﻿Shader "Unlit/GPUHashVisualization"
+{
+    Properties{ }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "Assets/GPUHash/Common.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            float4 frag (v2f i) : SV_Target
+            {
+                float c = pcg(i.vertex.x) * pcg(i.vertex.y) / float(0xffffffffu);
+                return float4(c, c, c, 1.0);
+            }
+            ENDCG
+        }
+    }
+}
